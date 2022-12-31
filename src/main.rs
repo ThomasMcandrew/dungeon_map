f8b8600f5948dbdf716b9613cb377eb71a666ba8
ThomasMcAndrew <thomasmca86@gmail.com>
2022-12-31 15:16:01 -0600

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,80 @@
-use druid::widget::Container;
-use druid::
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::
     {
-        AppLauncher, 
-        Data, 
-        Lens, 
-        LocalizedString, 
-        Widget, 
-        WindowDesc, 
-        ImageBuf,
-        im::Vector, 
+        Application,
+        ApplicationWindow,
+        Widget,
+        ListBox,
+        ScrolledWindow,
+        ListBoxRow,
+        Button,
+        FlowBox,
+        FlowBoxChild,
+        Image, 
+        DrawingArea,
+        
     };
 
-use druid::piet::ImageFormat;
-use imagesize::size;
-use std::path::Path;
-
-
-const WINDOW_TITLE: LocalizedString<ApplicationState> = 
-    LocalizedString::new("Hello World!");
-
-mod presentation;
-mod delegate;
-mod test;
-
-#[derive(Clone, Data, Lens)]
-pub struct ApplicationState {
-    pub name: String,
-    pub scenes: Vector<presentation::Scene>,
-    pub current_scene: presentation::Scene,
+fn idk() {
+    let draw = DrawingArea::new();
+    let test = Image::from_file("sky-background.png"); 
+    
 }
 
+fn map_screen() -> impl IsA<Widget> {
+    let test = Image::from_file("sky-background.png"); 
+
+    let scroll = ScrolledWindow::new();
+    scroll.set_child(Some(&test));
+    
+    scroll
+}
+
+fn root() -> impl IsA<Widget> {
+    let fbx = FlowBox::new();
+    
+    fbx.set_max_children_per_line(2);
+
+    let scenes = scenes_panel();
+    let scenes2 = map_screen();
+   
+    let fbc = FlowBoxChild::new();
+    fbc.set_child(Some(&scenes));
+    fbx.insert(&fbc,1);
+   
+    let fbc2 = FlowBoxChild::new();
+    fbx.insert(&fbc2,1);
+    fbc2.set_child(Some(&scenes2));
+  
+    fbx
+}
+
+fn scenes_panel() -> impl IsA<Widget> {
+    let list = ListBox::new();
+    let scroll = ScrolledWindow::new();
+
+    for _ in 0..80 {
+        let button = Button::with_label("Click me!");
+        let lbr = ListBoxRow::new();
+        lbr.set_child(Some(&button));
+        list.append(&lbr);
+    }
+    scroll.set_child(Some(&list));
+    scroll
+}
 fn main() {
-    // describe the main window
-    let main_window = WindowDesc::new(test::root_widget())
-        .window_size((400.0, 400.0));
-    let app_state = test::AppState {
-        image_buf: test::load_image_by_path_new("/home/thomas/dungeon_map/maps/ex2.jpg")
-    };
-    // start the application
-    AppLauncher::with_window(main_window)
-        .launch(app_state)
-        .expect("Failed to launch application");
-}
-
-//fn main() {
-//    // describe the main window
-//    let main_window = WindowDesc::new(build_root_widget())
-//        .title(WINDOW_TITLE)
-//        .window_size((400.0, 400.0));
-//    // create the initial app state
-//    let initial_state = ApplicationState {
-//        name: "World".into(),
-//        current_scene: presentation::Scene{
-//                    id: -1,
-//                    name: "default".into(),
-//                    created_date: "cd".into(),
-//                    updated_date: "ud".into(),
-//                    full_image: None,
-//                },
-//        scenes: Vector::from(vec!(
-//            presentation::Scene::new("one".into()),
-//            presentation::Scene::new("two".into()),
-//            presentation::Scene::new("three".into()),
-//            presentation::Scene::new("four".into()),
-//            presentation::Scene::new("five".into())
-//        )),
-//    };
-//
-//    // start the application
-//    AppLauncher::with_window(main_window)
-//        .delegate(delegate::Delegate {})
-//        .launch(initial_state)
-//        .expect("Failed to launch application");
-//}
-
-fn build_root_widget() -> impl Widget<ApplicationState> {
-    let container = Container::new(presentation::build_presentation());
-    container
-}
-/*
- * we want to make a save and load in the scene struct
- * then make a button that adds one so we can see if they are 
- * loading and saving correctly, then adding one switches the root
- * widget to the builder and right now just has name and save button 
- * stuff
- * */
-pub fn load_image() -> ImageBuf {
-    let path = Path::new("/home/thomas/dungeon_map/maps/ex2.jpg");
-    
-    let image_bytes = image::io::Reader::open(path)
-        .unwrap()
-        .decode()
-        .unwrap();
-    
-    let bytes = image_bytes.as_bytes();
-    
-    let (width, height) = match 
-        size(path) {
-            Ok(dim) => (dim.width, dim.height),
-            Err(_) => (0,0) 
-    };
-    ImageBuf::from_raw(
-        bytes,
-        ImageFormat::Rgb,
-        width,
-        height
-    )
+    let application = Application::builder()
+        .application_id("com.example.dungeon_map")
+        .build();
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("dmap")
+            .build();
+        let root = root();
+        window.set_child(Some(&root));
+        window.show();
+    });
+    application.run();
 }
