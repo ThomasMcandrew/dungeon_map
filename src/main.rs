efc8a62226cb7ce852bff6f89c8093e5538767ef
ThomasMcAndrew <thomasmca86@gmail.com>
2023-01-03 08:14:21 -0600

    initial

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,59 @@ use gtk::
     };
 use gtk::gdk_pixbuf::*;
 
+struct SimpleImage {
+    area: DrawingArea,
+    width: i32,
+    height: i32,
+    sprite_width: i32,
+    sprite_height: i32,
+    im_vec: Vec<Pixbuf>,
+}
+impl SimpleImage {
+    pub fn from_image(image: Pixbuf, w: i32, h: i32) -> Self {
+        let sprite_w = image.width() / w;
+        let sprite_h = image.height() / h;
+        let mut images_vec : Vec<Pixbuf> = Vec::new();
+        for x in 0..w {
+            for y in 0..h {
+                let i = image.new_subpixbuf(x,y,sprite_w,sprite_h).expect("invalid subpicture");
+                images_vec.push(i);
+            }
+        }
+        
+        let draw = DrawingArea::new();
+        draw.set_draw_func(|da: &DrawingArea,c: &Context,w: i32,h: i32|{
+            c.set_line_width(1.);
+            
+            for i in 0..images_vec.len() {
+                let si = images_vec[i];
+                c.set_source_pixbuf(&si,
+                    (sprite_w * (i as i32 / w)).into(),
+                    (sprite_h * (i as i32 / h)).into());
+            }
+    
+    
+            c.paint().expect("Failed to paint");
+        });
+        SimpleImage {
+            area: DrawingArea::new(),
+            width: w,
+            height: h,
+            sprite_width: sprite_w,
+            sprite_height: sprite_h,
+        }
+    }
+    pub fn new() -> Self {
+        SimpleImage {
+            area: DrawingArea::new(),
+            width: 0,
+            height: 0,
+            sprite_width: 0,
+            sprite_height: 0,
+        }
+    }
+}
+
 fn idk() -> impl IsA<Widget> {
     let draw = DrawingArea::new();
     draw.set_draw_func(|da: &DrawingArea,c: &Context,w: i32,h: i32|{
