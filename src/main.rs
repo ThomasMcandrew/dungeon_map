97ec313d11fa050a925380d91a4c67f61755b7b4
ThomasMcAndrew <thomasmca86@gmail.com>
2023-01-11 08:34:00 -0600

    initial

--- src/main.rs
+++ src/main.rs
@@ -25,77 +25,48 @@ struct SimpleImage {
     sprite_width: i32,
     sprite_height: i32,
 }
-impl SimpleImage {
-    pub fn from_image(image: Pixbuf, w: i32, h: i32) -> Self {
-        let sprite_w = image.width() / w;
-        let sprite_h = image.height() / h;
-        let mut images_vec : Vec<Pixbuf> = Vec::new();
-        for x in 0..w {
-            for y in 0..h {
-                let i = image.new_subpixbuf(x,y,sprite_w,sprite_h).expect("invalid subpicture");
-                images_vec.push(i);
-            }
+
+pub fn get_drawing_area_from_image(image: Pixbuf, width: i32, height: i32) -> DrawingArea {
+    let sprite_w = image.width() / width;
+    let sprite_h = image.height() / height;
+    let mut images_vec : Vec<Pixbuf> = Vec::new();
+    for y in 0..height {
+        for x in 0..width {
+            let i = image.new_subpixbuf(
+                    x * sprite_w,
+                    y * sprite_h,
+                    sprite_w,
+                    sprite_h)
+                .expect("invalid subpicture");
+            images_vec.push(i);
         }
+    }
+    
+    let draw = DrawingArea::new();
+
+    draw.set_draw_func(move |_da: &DrawingArea,c: &Context,w: i32,h: i32|{
+        c.set_line_width(1.);
         
-        let draw = DrawingArea::new();
-        draw.set_draw_func(move |_da: &DrawingArea,c: &Context,w: i32,h: i32|{
-            c.set_line_width(1.);
-            
-            for i in 0..images_vec.len() {
-                let si = images_vec[i].clone();
+        for y in 0..height {
+            for x in 0..width {
+                let si = images_vec[((y * width) + x) as usize].clone();
                 c.set_source_pixbuf(&si,
-                    (sprite_w * (i as i32 / w)).into(),
-                    (sprite_h * (i as i32 / h)).into());
+                    (x * sprite_w).into(),
+                    (y * sprite_h).into());
+
                 c.paint().expect("Failed to paint");
             }
-    
-    
-        });
-        SimpleImage {
-            area: DrawingArea::new(),
-            width: w,
-            height: h,
-            sprite_width: sprite_w,
-            sprite_height: sprite_h,
         }
-    }
-    pub fn new() -> Self {
-        SimpleImage {
-            area: DrawingArea::new(),
-            width: 0,
-            height: 0,
-            sprite_width: 0,
-            sprite_height: 0,
-        }
-    }
-}
-
-fn idk() -> impl IsA<Widget> {
-    let draw = DrawingArea::new();
-    draw.set_draw_func(|da: &DrawingArea,c: &Context,w: i32,h: i32|{
-        c.set_line_width(1.);
-
-        let pbx = Pixbuf::from_file("sky-background.png").expect("");
-        let foo = pbx.new_subpixbuf(32,32,32,32).expect("failed to get subpix");
-
-        c.set_source_pixbuf(&foo,0.,32.);
-
-        c.paint().expect("Failed to paint");
     });
-    
-    let display = gtk::gdk::Display::default().expect("No Default display");
-    let im = ImageSurface::create(Format::ARgb32,32,32).expect("Invalid surface");
     draw
 }
 
 fn map_screen() -> impl IsA<Widget> {
-    // let test = Image::from_file("sky-background.png"); 
-
-    // let scroll = ScrolledWindow::new();
-    // scroll.set_child(Some(&test));
-    // 
-    // scroll
-    idk()
+    let pbx = Pixbuf::from_file("cr.jpg").expect("");
+    let scroll = ScrolledWindow::new();
+    let si = get_drawing_area_from_image(pbx,10,10);
+    scroll.set_child(Some(&si));
+    scroll
 }
 
 fn root() -> impl IsA<Widget> {
